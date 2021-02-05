30688cdc1c7bdc94947b4fd2326d9bb210ae8b3d
Danny Brown <danny.ri.brown@gmail.com>
2021-02-05 16:30:45 -0500

    feat(libjpeg-turbojs): add libjpeg-turbojs package

--- packages/libjpeg-turbojs/build.sh
+++ packages/libjpeg-turbojs/build.sh
@@ -5,15 +5,17 @@ mkdir -p build
 mkdir -p dist
 
 # DEBUG CONFIGURE
-#(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..)
+#(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 
 echo "~~~ CONFIGURE ~~~"
 (cd build && emconfigure cmake ..)
 echo "~~~ MAKE ~~~"
 (cd build && emmake make VERBOSE=1 -j 16)
 echo "~~~ COPY ~~~ "
-cp ./build/src/charlsjs.js ./dist
-cp ./build/src/charlsjs.wasm ./dist
+cp ./build/src/libjpegturbowasm.js ./dist
+cp ./build/src/libjpegturbowasm.wasm ./dist
+cp ./build/src/libjpegturbojs.js.mem ./dist
+cp ./build/src/libjpegturbojs.js ./dist
 
 echo "~~~ BUILD:"
 (cd build && dir)
