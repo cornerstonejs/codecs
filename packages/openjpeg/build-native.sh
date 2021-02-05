f6b140ccd315959a7d57a424c3d2f5aee64ec3fd
dannyrb <danny.ri.brown@gmail.com>
2021-02-04 20:44:32 -0500

    tests and build command changes

--- packages/openjpeg/build-native.sh
+++ packages/openjpeg/build-native.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p build-native
 (cd build-native && cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
+#(cd build-native && cmake ..) &&
 (cd build-native && make VERBOSE=1 -j 8) &&
 #(build-native/test/cpp/cpptest) &&
 (build-native/extern/openjpeg/bin/cpptest)
--- packages/openjpeg/build.sh
+++ packages/openjpeg/build.sh
@@ -2,28 +2,11 @@
 # Disable exit on non 0
 set +e
 mkdir -p build
-#(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-
-# This worked in: trzeci/emscripten:1.39.4-fastcomp
-# but not w/ newer versions of emscripten
-# (cd build && emconfigure cmake ..) &&
-# ~ Build Native
-# (cd build && cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-# (cd build && make VERBOSE=1 -j 8) &&
-# (build/extern/openjpeg/bin/cpptest)
-
-# ~ Build WASM/JS
-# (cd build && emcmake cmake ..) || echo 0 &&
-# (cd build && emcmake cmake ..) &&
-# (cd build && emmake make VERBOSE=1 -j 16) &&
-# cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
-# cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
-# cp ./build/extern/openjpeg/bin/openjpegwasm.js ./dist &&
-# cp ./build/extern/openjpeg/bin/openjpegwasm.wasm ./dist &&
-# (cd test/node; npm run test)
-
-(cd build && emconfigure cmake ..)
-(cd build && emmake make VERBOSE=1 -j 16)
-cp ./build/src/openjpegjs.js ./dist
-cp ./build/src/openjpegjs.wasm ./dist
-# (cd test/node; npm run test)
+#(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
+(cd build && emconfigure cmake ..) &&
+(cd build && emmake make VERBOSE=1 -j 16) &&
+cp ./build/extern/openjpeg/bin/openjpegjs.js ./dist && 
+cp ./build/extern/openjpeg/bin/openjpegjs.js.mem ./dist &&
+cp ./build/extern/openjpeg/bin/openjpegwasm.js ./dist &&
+cp ./build/extern/openjpeg/bin/openjpegwasm.wasm ./dist &&
+(cd test/node; npm run test)
