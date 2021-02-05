c6e11acdb70f1f324225cb9687e8a6a484935858
dannyrb <danny.ri.brown@gmail.com>
2021-02-04 23:03:46 -0500

    test charls-js

--- packages/charls-js/build.sh
+++ packages/charls-js/build.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+# Disable exit on non 0
+set +e
 mkdir -p build
+mkdir -p dist
+
+# DEBUG CONFIGURE
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..)
+
+echo "~~~ CONFIGURE ~~~" &&
 (cd build && emconfigure cmake ..)
+echo "~~~ MAKE ~~~" &&
 (cd build && emmake make VERBOSE=1 -j 16)
+echo "~~~ COPY ~~~ " &&
 cp ./build/src/charlsjs.js ./dist
 cp ./build/src/charlsjs.wasm ./dist
-(cd test/node; npm run test)
+
+echo "~~~ BUILD:"
+(cd build && dir)
+echo "~~~ DIST:"
+(cd dist && dir)
