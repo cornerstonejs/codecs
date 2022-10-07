e42978b108f40e4c1d676c99651a54fa48e2ab0a
Jason Hostetter <jhostetter@gmail.com>
2022-10-07 09:52:18 -0400

    Add openjphjs package (#12)
    Agreed with Chris that this could be merged.

---
+++ packages/openjphjs/build/extern/OpenJPH/CMakeFiles/ojph_compress.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../../bin/ojph_compress.js"
+  "../../../bin/ojph_compress.pdb"
+  "CMakeFiles/ojph_compress.dir/src/apps/ojph_compress/ojph_compress.cpp.o"
+  "CMakeFiles/ojph_compress.dir/src/apps/others/ojph_img_io.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ojph_compress.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/openjphjs/build/extern/OpenJPH/CMakeFiles/openjphsimd.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openjphsimd.dir/src/core/codestream/ojph_codestream.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/codestream/ojph_codestream_wasm.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/codestream/ojph_params.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/coding/ojph_block_common.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/coding/ojph_block_decoder.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/coding/ojph_block_decoder_wasm.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/coding/ojph_block_encoder.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/others/ojph_arch.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/others/ojph_file.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/others/ojph_mem.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/others/ojph_message.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/transform/ojph_colour.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/transform/ojph_colour_wasm.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/transform/ojph_transform.cpp.o"
+  "CMakeFiles/openjphsimd.dir/src/core/transform/ojph_transform_wasm.cpp.o"
+  "libopenjphsimd.a"
+  "libopenjphsimd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openjphsimd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
