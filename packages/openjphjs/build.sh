a6c600896736db8d2b0ce919bc57b6b11a501d17
Bill Wallace <wayfarer3130@gmail.com>
2023-10-24 09:36:07 -0400

    Revert to non-debug version

--- packages/openjphjs/build.sh
+++ packages/openjphjs/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p build
 mkdir -p dist
-(cd build && emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..)
-#(cd build && CXXFLAGS=-msimd128 emcmake cmake ..)
+#(cd build && CXXFLAGS=-msimd128 emcmake cmake -DCMAKE_BUILD_TYPE=Debug ..)
+(cd build && CXXFLAGS=-msimd128 emcmake cmake ..)
 (cd build && emmake make VERBOSE=1 -j ${nprocs})
 cp ./build/src/openjphjs.js ./dist
 cp ./build/src/openjphjs.wasm ./dist
